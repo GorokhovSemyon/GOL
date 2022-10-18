bf0a987d04a8b95f0f6d373dd0e96d553a8a7342
GorokhovSemyon <SamGorokhov@yandex.ru>
2022-10-18 22:57:56 +0300

    SG: initial commit

---
+++ src/drawings.h
@@ -0,0 +1,10 @@
+#ifndef GOL_DRAWINGS_H
+#define GOL_DRAWINGS_H
+
+void drawGreeting();
+void drawGameOver();
+void drawLiveDeath();
+void drawInfinity();
+void drawManual();
+
+#endif //GOL_DRAWINGS_H
